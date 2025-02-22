8193ecbac06c72887e848e4f0a74fb4dac8a1c02
Ali Sleiman <asleiman@acksession.com>
2025-02-22 13:39:23 +0200

    services

---
+++ Portfolio.Services/Helpers/DataSeeder.cs
@@ -0,0 +1,15 @@
+using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.Extensions.DependencyInjection;
+
+public static class DataSeeder
+{
+    public static void SeedData(IServiceProvider serviceProvider)
+    {
+       
+          
+    }
+}
